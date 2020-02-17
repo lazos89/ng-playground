23d5d3632eb28858d3fc05d45d0f68712e2cd01a
Lazos Doug <lazarosd89@gmail.com>
2020-02-17 03:00:13 +0200

    create components

--- projects/typings-example-app/src/app/app.module.ts
+++ projects/typings-example-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ElementsModule } from "../../../components/src/app/elements/elements.module";
+import { ViewsModule } from "../../../components/src/app/views/views.module";
+import { ModsModule } from "../../../components/src/app/mods/mods.module";
+import { CollectionsModule } from "../../../components/src/app/collections/collections.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ElementsModule,
+    ViewsModule,
+    ModsModule,
+    CollectionsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
