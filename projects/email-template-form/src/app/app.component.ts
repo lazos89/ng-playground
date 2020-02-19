f8646db18ed9e7fe9d83a43c2c0fe5a0e9438c2b
Lazos Doug <lazarosd89@gmail.com>
2020-02-19 14:34:55 +0200

    email-template-form

---
+++ projects/email-template-form/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  email: string;
+  onSubmit() {
+    console.log(this.email);
+  }
+}
