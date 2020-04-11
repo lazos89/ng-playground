4f11a317e8eaf105fc484d4162c262c81aa48b30
Lazos Doug <lazarosd89@gmail.com>
2020-04-11 10:12:26 +0300

    [Products App] login component + font awasome icons

--- projects/products-app/src/app/app-routing.module.ts
+++ projects/products-app/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { WelcomeComponent } from "./home/welcome.component";
 import { PageNotFoundComponent } from "./shared/page-not-found/page-not-found.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 const routes: Routes = [
+  { path: "login", component: LoginComponent },
   { path: "welcome", component: WelcomeComponent },
   { path: "", pathMatch: "full", redirectTo: "welcome" },
   { path: "**", component: PageNotFoundComponent },
